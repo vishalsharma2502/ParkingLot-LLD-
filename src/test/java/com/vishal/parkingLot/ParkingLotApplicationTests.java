0bc6675ea746b25973b785c37b3d051e4e733d41
Vishal <postme.sharmaji@gmail.com>
2021-06-12 23:49:05 +0530

    initial first commit

---
+++ src/test/java/com/vishal/parkingLot/ParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.parkingLot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
